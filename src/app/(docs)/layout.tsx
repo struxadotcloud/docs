020174aaecd5dcd1141d4d7167556e0fc6cc7bc1
stripsior <jakubkrzyz.kontakt@gmail.com>
2026-05-31 01:46:38 +0200

    feat: add Extensions docs tab with notebook layout

--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -1,10 +1,18 @@
 import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout
+      tree={source.getPageTree()}
+      {...baseOptions()}
+      tabMode="navbar"
+      tabs={[
+        { title: 'Docs', url: '/docs' },
+        { title: 'Extensions', url: '/docs/extensions' },
+      ]}
+    >
       {children}
     </DocsLayout>
   );
